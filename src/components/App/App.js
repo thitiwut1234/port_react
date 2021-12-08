bf277f0689626b34457a1637435935dbd206d329
T168 <thitiwut.tra@gmail.com>
2021-12-08 13:24:02 +0700

    Create Homepage and install TypeEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavigatorBar from '../Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import DomainHome from '../../domains/DomainHome'
+
+
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
         <NavigatorBar />
       </header>
-
+      <content className='App-content' >
+      <Routes>
+        <Route path='/' element={<DomainHome /> } />
+      </Routes>
+      </content>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/domains/DomainHome/DomainHome.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const DomainHome = () => {
+
+    return (
+        <h1> DomainHome </h1>
+    )
+}
+
+export default DomainHome
